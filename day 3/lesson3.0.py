a1a5588b7d36947357e62731038bc4899f83f2c4
tughushinana <tughushinanuka@gmail.com>
2024-02-13 23:36:43 +0400

    third commit

--- day 3/lesson3.0.py
+++ day 3/lesson3.0.py
@@ -2,7 +2,22 @@ print("ekaterine mdivani")
 
 #this is my mother
 
-
-
+print("beqa tughushi")
 
 #this is my father
+
+print("nini tughushi")
+
+#this is my older sister
+
+print("nanuka tughushi")
+
+#this is me
+
+print("ana tughushi")
+
+#this is my younger sister
+
+print("anton tughushi")
+
+#this is the youngest sibling he's name is anton

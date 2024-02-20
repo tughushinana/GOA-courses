eca72125c51a511e2df9ee40d71969c0a50340ac
tughushinana <tughushinanuka@gmail.com>
2024-02-20 21:26:20 +0400

    fifth commit

--- day 5/homework/lesson 5.py
+++ day 5/homework/lesson 5.py
@@ -6,4 +6,8 @@ my_age = 12
 lil_sisters_age = 4
 bros_age = 2
 print( "my mom is " +str(moms_age) + ", my dad is " + str(dads_age) + ", my older sister is " + str(sisters_age) + 
-      ", i'm " + str(my_age) + ", my litle sister is " + str(lil_sisters_age) + " and my little brother is " + str(bros_age)  )
+      ", i'm " + str(my_age) + ", my litle sister is " + str(lil_sisters_age) + " and my little brother is " + str(bros_age) )
+
+print( "my family members in five years are gonna be: mom  " +str(moms_age + 5 ) + ",  dad " + str(dads_age+ 5) 
+      + ",  older sister  " + str(sisters_age + 5) + 
+      ", i will be " + str(my_age+ 5) + ", litle sister " + str(lil_sisters_age+ 5) + " and little brother  " + str(bros_age+ 5))

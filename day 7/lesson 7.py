f7204bb3944bff39f141b035224a10ce99424b8b
tughushinana <tughushinanuka@gmail.com>
2024-02-26 01:10:28 +0400

    seventh commit

---
+++ day 7/lesson 7.py
@@ -0,0 +1,13 @@
+nanuka = int(input("enter nanukas age : "))
+jeko = int(input("enter jekos age :"))
+leqso =int(input("enter leqsos age :"))
+barbare =int(input("enter barbares age:"))
+batoni_davit_janezashvili =int(input("enter batoni davit janezashvilis age :"))
+multiplication = nanuka * jeko
+print("multiplication answer is " + str(multiplication))
+division = multiplication // leqso
+print("division answer is " + str(division))
+addition = barbare + batoni_davit_janezashvili
+print("addition answer is " + str(addition))
+final_answer = (multiplication + division ) * addition
+print("final answer is " + str(final_answer))
---
+++ day 7/lesson7.py
@@ -0,0 +1,7 @@
+#in the gaming store there is a VARIETY of computers
+#the answer was INCORRECT
+#i LOST the presentation becouze i didnt save it
+#SELECT one of the answers
+#she didnt HANDLE the news very well
+#did you TELL your mom adout the school trip
+# will you answer tghis small SURVEY

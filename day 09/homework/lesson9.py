70b68b0761dd9e6b1163df795c926d45eb9c8103
tughushinana <tughushinanuka@gmail.com>
2024-03-04 00:12:30 +0400

    commit

---
+++ day 09/homework/lesson9.py
@@ -0,0 +1,35 @@
+print(True or False)
+#True
+
+print(False or True)
+#False
+
+print(True and False)
+#False
+
+print(False and True)
+#True
+
+addition = True
+division = False
+
+print( addition or division)
+
+print( addition and division)
+
+
+multiplication = True 
+multiplication = False
+
+print(multiplication or multiplication )
+
+print(multiplication and multiplication )
+
+
+first_answer = True
+second_answer = False
+
+print(first_answer or second_answer)
+print(first_answer and second_answer)
+
+
